f3a3706390a3517588e09f15bd52bd273230e276
dazzlear <dazel.argallon7@gmail.com>
2025-12-23 12:25:22 +0800

    Create initial word counter program

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("WORD COUNTER (Words only)");
+        Console.WriteLine("Type a sentence to count words.");
+        Console.WriteLine("Type 'exit' to quit.\n");
+
+        while (true)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine() ?? "";
+
+            if (input.Trim().Equals("exit", StringComparison.OrdinalIgnoreCase))
+                break;
+
+            // Count words only: split by whitespace and remove empty entries
+            int wordCount = input
+                .Split((char[])null, StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+
+            Console.WriteLine($"Word count: {wordCount}\n");
+        }
+
+        Console.WriteLine("Goodbye!");
+    }
+}
